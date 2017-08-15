6dc8957f01ecc9ab9db46302c75cdb1b8b1e7166
Adriaan de Jonge <adriaandejonge@gmail.com>
2017-08-15 20:18:55 +0400

    Add unit test for dirForFile()

--- dump/worker_test.go
+++ dump/worker_test.go
@@ -5,6 +5,23 @@ import (
 	"testing"
 )
 
+func TestDirForFile(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"/basic/file.txt", "/basic"},
+		{"./one/more/file.txt", "./one/more"},
+		{"./one/less/", "./one/less"},
+	}
+	for _, test := range tests {
+		result := dirForFile(test.input)
+		if result != test.expected {
+			t.Errorf("dirForFile(%v) incorrect, expected %v, got %v", test.input, test.expected, result)
+		}
+	}
+}
+
 func TestConvertContent(t *testing.T) {
 	tests := []struct {
 		Map      map[string]interface{}
