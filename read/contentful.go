c12ad62d37a010b0512774b73d1a54d6876b004d
Patrick Scott <patrick.scott@bhspecialty.com>
2018-04-27 11:36:11 -0400

    refactoring contentful.go for reuse

--- read/contentful.go
+++ read/contentful.go
@@ -13,24 +13,28 @@ type Contentful struct {
 	ReadConfig ReadConfig
 }
 
+// Types will use Contentful's content_types endpoint to retrieve all content types from contentful
 func (c *Contentful) Types() (rc io.ReadCloser, err error) {
-	urlBase := URL
-	if c.ReadConfig.UsePreview {
-		urlBase = previewURL
-	}
-	return c.Getter.Get(urlBase + "/spaces/" +
+
+	return c.get("/spaces/" +
 		c.ReadConfig.SpaceID + "/content_types?access_token=" +
 		c.ReadConfig.AccessToken + "&limit=200&locale=" +
 		c.ReadConfig.Locale)
 }
 
+// Items will use Contentful's entires endpoint to retrieve all 'items' from contetnful
 func (c *Contentful) Items(skip int) (rc io.ReadCloser, err error) {
+
+	return c.get("/spaces/" +
+		c.ReadConfig.SpaceID + "/entries?access_token=" +
+		c.ReadConfig.AccessToken + "&limit=200&locale=" +
+		c.ReadConfig.Locale + "&skip=" + strconv.Itoa(skip))
+}
+
+func (c *Contentful) get(endpoint string) (rc io.ReadCloser, err error) {
 	urlBase := URL
 	if c.ReadConfig.UsePreview {
 		urlBase = previewURL
 	}
-	return c.Getter.Get(urlBase + "/spaces/" +
-		c.ReadConfig.SpaceID + "/entries?access_token=" +
-		c.ReadConfig.AccessToken + "&limit=200&locale=" +
-		c.ReadConfig.Locale + "&skip=" + strconv.Itoa(skip))
+	return c.Getter.Get(urlBase + endpoint)
 }
